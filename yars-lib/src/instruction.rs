224818e9ccea3476a0ba814464c0a54a0ff95ed4
Leonardo H. Neumann <leonardohn@null.net>
2020-09-29 23:13:35 -0300

    yars-lib: Implemented instruction decoder

--- yars-lib/src/instruction.rs
+++ yars-lib/src/instruction.rs
@@ -244,3 +244,182 @@ impl fmt::Display for Instruction {
         }
     }
 }
+
+impl TryFrom<u32> for Instruction {
+    type Error = ();
+
+    fn try_from(inst: u32) -> Result<Self, Self::Error> {
+        let opcode = (inst & 0x7F) as u8;
+        let format = InstructionFormat::from_opcode(opcode).ok_or(())?;
+
+        match format {
+            InstructionFormat::R => {
+                let rd = ((inst >> 7) & 0b11111) as u8;
+                let rs1 = ((inst >> 15) & 0b11111) as u8;
+                let rs2 = ((inst >> 20) & 0b11111) as u8;
+                let funct3 = ((inst >> 12) & 0b111) as u8;
+                let funct7 = ((inst >> 25) & 0b1111111) as u8;
+                let fn3_opcode = (funct3 << 5) | (opcode >> 2);
+
+                match fn3_opcode {
+                    0b000_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::ADD { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::MUL { rd, rs1, rs2 }),
+                        0b0100000 => Ok(Instruction::SUB { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    0b001_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::SLL { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::MULH { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    0b010_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::SLT { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::MULHSU { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    0b011_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::SLTU { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::MULHU { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    0b100_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::XOR { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::DIV { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    0b101_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::SRL { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::DIVU { rd, rs1, rs2 }),
+                        0b0100000 => Ok(Instruction::SRA { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    0b110_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::OR { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::REM { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    0b111_01100 => match funct7 {
+                        0b0000000 => Ok(Instruction::AND { rd, rs1, rs2 }),
+                        0b0000001 => Ok(Instruction::REMU { rd, rs1, rs2 }),
+                        _ => Err(()),
+                    },
+                    _ => Err(()),
+                }
+            }
+            InstructionFormat::R4 => Err(()),
+            InstructionFormat::I => {
+                let rd = ((inst >> 7) & 0b11111) as u8;
+                let rs1 = ((inst >> 15) & 0b11111) as u8;
+                let funct3 = ((inst >> 12) & 0b111) as u8;
+                let imm = ((inst as i32) >> 20) as i16;
+                let csr = imm as u16 & 0b111111111111;
+                let fn3_opcode = (funct3 << 5) | (opcode >> 2);
+
+                match fn3_opcode {
+                    0b000_11001 => Ok(Instruction::JALR { rd, rs1, imm }),
+                    0b000_00000 => Ok(Instruction::LB { rd, rs1, imm }),
+                    0b001_00000 => Ok(Instruction::LH { rd, rs1, imm }),
+                    0b010_00000 => Ok(Instruction::LW { rd, rs1, imm }),
+                    0b100_00000 => Ok(Instruction::LBU { rd, rs1, imm }),
+                    0b101_00000 => Ok(Instruction::LHU { rd, rs1, imm }),
+                    0b000_00100 => Ok(Instruction::ADDI { rd, rs1, imm }),
+                    0b010_00100 => Ok(Instruction::SLTI { rd, rs1, imm }),
+                    0b011_00100 => Ok(Instruction::SLTIU { rd, rs1, imm }),
+                    0b100_00100 => Ok(Instruction::XORI { rd, rs1, imm }),
+                    0b110_00100 => Ok(Instruction::ORI { rd, rs1, imm }),
+                    0b111_00100 => Ok(Instruction::ANDI { rd, rs1, imm }),
+                    0b000_00011 => {
+                        let fm = ((imm >> 8) & 0b1111) as u8;
+                        let pred = ((imm >> 4) & 0b1111) as u8;
+                        let succ = (imm & 0b1111) as u8;
+                        let pred = FenceKind::try_from(pred)?;
+                        let succ = FenceKind::try_from(succ)?;
+
+                        match (fm, pred, succ) {
+                            (0b1000, FenceKind::RW, FenceKind::RW) => Ok(Instruction::FENCETSO),
+                            (0b0000, pred, succ) => Ok(Instruction::FENCE { pred, succ }),
+                            _ => Err(()),
+                        }
+                    }
+                    0b000_11100 => match imm {
+                        0 => Ok(Instruction::ECALL),
+                        1 => Ok(Instruction::EBREAK),
+                        _ => Err(()),
+                    },
+                    0b001_00011 => Ok(Instruction::FENCEI),
+                    0b001_11100 => Ok(Instruction::CSRRW { rd, rs1, csr }),
+                    0b010_11100 => Ok(Instruction::CSRRS { rd, rs1, csr }),
+                    0b011_11100 => Ok(Instruction::CSRRC { rd, rs1, csr }),
+                    0b101_11100 => Ok(Instruction::CSRRWI { rd, uimm: rs1, csr }),
+                    0b110_11100 => Ok(Instruction::CSRRSI { rd, uimm: rs1, csr }),
+                    0b111_11100 => Ok(Instruction::CSRRCI { rd, uimm: rs1, csr }),
+                    _ => Err(()),
+                }
+            }
+            InstructionFormat::S => {
+                let funct3 = ((inst >> 12) & 0b111) as u8;
+                let rs1 = ((inst >> 15) & 0b11111) as u8;
+                let rs2 = ((inst >> 20) & 0b11111) as u8;
+                let imm115 = (inst >> 25) & 0b1111111;
+                let imm40 = (inst >> 7) & 0b11111;
+                let imm = (imm115 << 5) | imm40;
+                let imm = ((imm as i16) << 4) >> 4;
+                let fn3_opcode = (funct3 << 5) | (opcode >> 2);
+
+                match fn3_opcode {
+                    0b000_01000 => Ok(Instruction::SB { rs1, rs2, imm }),
+                    0b001_01000 => Ok(Instruction::SH { rs1, rs2, imm }),
+                    0b010_01000 => Ok(Instruction::SW { rs1, rs2, imm }),
+                    _ => Err(()),
+                }
+            }
+            InstructionFormat::B => {
+                let funct3 = ((inst >> 12) & 0b111) as u8;
+                let rs1 = ((inst >> 15) & 0b11111) as u8;
+                let rs2 = ((inst >> 20) & 0b11111) as u8;
+                let imm12 = (inst >> 31) & 0b1;
+                let imm105 = (inst >> 25) & 0b111111;
+                let imm41 = (inst >> 8) & 0b1111;
+                let imm11 = (inst >> 7) & 0b1;
+                let imm = (imm12 << 12) | (imm11 << 11) | (imm105 << 5) | (imm41 << 1);
+                let imm = ((imm as i16) << 3) >> 3;
+                let fn3_opcode = (funct3 << 5) | (opcode >> 2);
+
+                match fn3_opcode {
+                    0b000_11000 => Ok(Instruction::BEQ { rs1, rs2, imm }),
+                    0b001_11000 => Ok(Instruction::BNE { rs1, rs2, imm }),
+                    0b100_11000 => Ok(Instruction::BLT { rs1, rs2, imm }),
+                    0b101_11000 => Ok(Instruction::BGE { rs1, rs2, imm }),
+                    0b110_11000 => Ok(Instruction::BLTU { rs1, rs2, imm }),
+                    0b111_11000 => Ok(Instruction::BGEU { rs1, rs2, imm }),
+                    _ => Err(()),
+                }
+            }
+            InstructionFormat::U => {
+                let rd = ((inst >> 7) & 0b11111) as u8;
+                let imm = (inst & !0b111111111111) as i32;
+
+                match opcode >> 2 {
+                    0b01101 => Ok(Instruction::LUI { rd, imm }),
+                    0b00101 => Ok(Instruction::AUIPC { rd, imm }),
+                    _ => Err(()),
+                }
+            }
+            InstructionFormat::J => {
+                let rd = ((inst >> 7) & 0b11111) as u8;
+                let imm20 = (inst >> 31) & 0b1;
+                let imm101 = (inst >> 21) & 0b1111111111;
+                let imm11 = (inst >> 20) & 0b1;
+                let imm1912 = (inst >> 12) & 0b11111111;
+                let imm = (imm20 << 20) | (imm1912 << 12) | (imm11 << 11) | (imm101 << 1);
+                let imm = ((imm as i32) << 11) >> 11;
+
+                match opcode >> 2 {
+                    0b11011 => Ok(Instruction::JAL { rd, imm }),
+                    _ => Err(()),
+                }
+            }
+        }
+    }
+}

f20f54ce1a0dc8e4016656732b34ccc386b9e326
Leonardo H. Neumann <leonardohn@null.net>
2020-10-01 17:06:49 -0300

    yars-cli: Implemented argument parsing

--- yars-cli/src/main.rs
+++ yars-cli/src/main.rs
@@ -1,2 +1,28 @@
+use clap::{Clap, crate_authors, crate_description, crate_version};
+use std::path::PathBuf;
+
+#[derive(Clap)]
+#[clap(name = "yars")]
+#[clap(author = crate_authors!())]
+#[clap(version = crate_version!())]
+#[clap(about = crate_description!())]
+struct Opts {
+    #[clap(short, long)]
+    #[clap(about = "Logs instruction execution")]
+    log: bool,
+
+    #[clap(short, long, value_name = "size", default_value = "128")]
+    #[clap(about = "Allocate <size> MiB for target memory")]
+    memory: usize,
+
+    #[clap(long, value_name = "address")]
+    #[clap(about = "Override program entry point")]
+    pc: Option<usize>,
+
+    #[clap(about = "Path to target RISC-V program")]
+    program: PathBuf,
+}
+
 fn main() {
+    let opts = Opts::parse();
 }
